57a6be761db51473dc76005b69bfc183b1a1ae86
Vaniot <12030332+vaniot-s@users.noreply.github.com>
2019-07-15 16:03:29 +0800

    Update process_linux.go

--- process_linux.go
+++ process_linux.go
@@ -11,7 +11,7 @@ import (
 // Refresh reloads all the data associated with this process.
 func (p *UnixProcess) Refresh() error {
 // 	statPath := fmt.Sprintf("/proc/%d/stat", p.pid)
-	statPath := `/proc/` + strconv.FormatUint(uint64(pid), 10) + `/comm`
+	statPath := fmt.Sprintf("/proc/%d/comm", p.pid)
 	dataBytes, err := ioutil.ReadFile(statPath)
 	if err != nil {
 		return err
